1341049eabefd14393cabef304897426b88c2f13
c-wx <1404668149@qq.com>
2021-04-21 22:12:50 +0800

    init project

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,45 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'blog-front',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'cbuc blog front' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+  server: {
+    port: 3000 // default: 3000
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+   ** Headers of the page
+   */
+  head: {
+    title: "小菜良记",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "meta-key-words",
+        name: "keywords",
+        content: "小菜良记：SQL, Spring, 分布式, 微服务, 高并发, 架构"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "小菜良记：SQL, Spring, 分布式, 微服务, 高并发, 架构"
       }
-    }
-  }
-}
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: { color: "#3B8070" },
 
+  modules: [
+    "@nuxtjs/axios" //引入axios模块
+  ],
+
+  env: {
+    BASE_API: "http://localhost"
+  },
+
+  axios: {
+    // Axios options here
+    baseURL: "http://localhost"
+  },
+
+  plugins: ["~/plugins/element-ui.js", "~/plugins/vue-qriously-plugin.js"]
+};
